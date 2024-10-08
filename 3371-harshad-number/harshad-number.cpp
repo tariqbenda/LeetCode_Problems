64ab47b5a861814152db0de9d8e2d346b7f75712
Tariq Ben-dallah <64340323+tariqbenda@users.noreply.github.com>
2024-10-08 18:13:23 +0100

    Time: 0 ms (100.00%) | Memory: 7.5 MB (61.23%) - LeetSync

---
+++ 3371-harshad-number/harshad-number.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+public:
+    int sumOfTheDigitsOfHarshadNumber(int x) {
+        std::string num = to_string(x);
+        int sum = 0;
+        for (int i = 0; i < num.length(); i++)
+            sum += num[i] - '0';
+        if (x % sum == 0)
+            return sum;
+        return -1;
+    }
+};
